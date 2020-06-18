faa7ce0dd5c150a9f4f15b49c807b454be730f27
Sinan Buyukkardes <sinanbuyukkardes@hotmail.com>
2020-06-18 15:03:15 +0200

    Create scale_to_480p.sh

---
+++ scale_to_480p.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Needed for file based operations
+. util.sh --source-only
+
+filename=$(get_filename $1)
+extension=$(get_extension $1)
+
+ffmpeg.exe -i $1 -s 720x480 $filename.480p.$extension
+
